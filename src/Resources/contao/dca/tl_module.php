5e5b896622ed4896916c9bc58add299e73b37055
Christine Buchsteiner <ch.buchsteiner@gmx.de>
2025-08-31 14:04:19 +0200

    change dca file

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -2,19 +2,19 @@
 
 /**
  * DCA configuration for the custom frontend module "split_navigation".
- * This module extends Contao's navigation module and allows injecting
- * an article between two navigation blocks.
+ * This module extends Contao's navigation module and adds a custom field
+ * to inject an article between two navigation blocks.
  */
 
-// Register a new palette for the module type "split_navigation"
+// Extend the existing palette "navigation" for your custom module type
 $GLOBALS['TL_DCA']['tl_module']['palettes']['split_navigation'] =
-    '{title_legend},name,headline,type;{navigation_legend},levelOffset,showLevel,hardLimit,showProtected,defineRoot,rootPage;{article_legend},headerArticleAlias;{template_legend:hide},navigationTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+    $GLOBALS['TL_DCA']['tl_module']['palettes']['navigation'] . ';{article_legend},headerArticleAlias';
 
-// Define the custom field "headerArticleAlias" used to inject an article between navigation blocks
+// Define the custom field "headerArticleAlias"
 $GLOBALS['TL_DCA']['tl_module']['fields']['headerArticleAlias'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_module']['headerArticleAlias'], // Label and description from language file
-    'exclude'   => true,                                                    // Exclude from search and export
-    'inputType' => 'text',                                                  // Simple text input
-    'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],               // Max length and CSS class for layout
-    'sql'       => "varchar(255) NOT NULL default ''"                       // Database definition
+    'label'     => &$GLOBALS['TL_LANG']['tl_module']['headerArticleAlias'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
+    'sql'       => "varchar(255) NOT NULL default ''"
 ];
